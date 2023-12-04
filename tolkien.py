ef99e4049aace1e51a32d2611877dca662b16b01
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2023-12-04 21:59:05 +0100

    Ad control flow for the user input If the search match more than one character the program will output the results of the search.

--- tolkien.py
+++ tolkien.py
@@ -5,5 +5,13 @@ from hero import Hero
 name = input("Name: ")
 
 data = requests.get(f"https://the-one-api.dev/v2/character?name=/^{name}.*/i", headers={"Authorization": "Bearer EUO-leXY6aL0ncP9-wmF"})
+data = data.json()
 
-print(json.dumps(data.json(),indent=2))
+# print(json.dumps(data,indent=2))
+if data["total"] > 1:
+    results = []
+    print("Many results, which one of them did you mean?")
+    for result in range(data["total"]):
+        results.append(data["docs"][result]["name"])
+    for _ in results:
+        print(_)
