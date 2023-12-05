3f10aa28e2bec5309e950183362cb4fe28e884ce
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2023-12-05 21:36:56 +0100

    Implement control for the user's search. With more than one result for the search the program will output an ordered list of the results and take an input(int) to choose the character.

--- tolkien.py
+++ tolkien.py
@@ -2,16 +2,45 @@ import requests
 import json
 from hero import Hero
 
-name = input("Name: ")
+def main():
+    name = input("Name: ")
+    while True:
+        if not name:
+            name = input("Name: ")
+            continue
+        else:
+            break
 
-data = requests.get(f"https://the-one-api.dev/v2/character?name=/^{name}.*/i", headers={"Authorization": "Bearer EUO-leXY6aL0ncP9-wmF"})
-data = data.json()
+    data = requests.get(f"https://the-one-api.dev/v2/character?name=/^{name}.*/i", headers={"Authorization": "Bearer EUO-leXY6aL0ncP9-wmF"})
+    data = data.json()
 
-# print(json.dumps(data,indent=2))
-if data["total"] > 1:
+    # print(json.dumps(data,indent=2))
+
+    # Decision maker if there is more than one resulto for the search
+    if data["total"] > 1:
+        number = get_decision(data)
+        print(json.dumps(data['docs'][number],indent=1))
+    else:
+        print(json.dumps(data['docs'][0],indent=1))
+
+
+def get_decision(data):
     results = []
-    print("Many results, which one of them did you mean?")
+    print(f"More than one result:\n")
     for result in range(data["total"]):
-        results.append(data["docs"][result]["name"])
+        results.append(f"{result+1}. {data['docs'][result]['name']}")
     for _ in results:
         print(_)
+    print("\n")
+    while True:
+        number = int(input("Enter the number of the character you want to search for: "))
+        if number in range(len(results)):
+            number -= 1
+            break
+        else:
+            continue
+    return number
+
+
+if __name__ == "__main__":
+    main()
