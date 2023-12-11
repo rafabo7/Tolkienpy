3acce606efd9a80333220cc5d0b3fe02936c8a6e
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2023-12-11 18:08:30 +0100

    The mvp is near Import the random and re libraries. Change the welcome message, acording to the new format of the programme: a mini-game. Add a control flow for the user to confirm the character they look for -> not finished. Add the text mini-game with asci art balrog and a function for the fight: The func checks an input and the race of the character.

--- tolkien.py
+++ tolkien.py
@@ -1,19 +1,63 @@
 import sys
 import requests
-import json
+import random
+import re
 from hero import Hero
 
 def main():
     print(
-        "\nWelcome to Tolkienpy, a modest programme to find character from the J.R.R Tolkien's universe.\n"
+        "\nWelcome to Tolkienpy, a modest textual game inspired in the J.R.R Tolkien's universe.\n"
         )
-    name = get_name()
+    while True:
+        name = get_name()
 
-    data = requests.get(f"https://the-one-api.dev/v2/character?name=/^{name}.*/i", headers={"Authorization": "Bearer EUO-leXY6aL0ncP9-wmF"})
-    data = data.json()
+        data = requests.get(f"https://the-one-api.dev/v2/character?name=/^{name}.*/i", headers={"Authorization": "Bearer EUO-leXY6aL0ncP9-wmF"})
+        data = data.json()
 
-    hero = get_hero(data)
-    print(hero)
+        hero = get_hero(data)
+        print(hero, "\n")
+        answer = input("This is your hero. Are you ready to continue?")
+        if matches := re.match(r"^ye?s?$", answer.strip(), re.IGNORECASE):
+            break
+        elif matches := re.match(r"^no?$", answer.strip(), re.IGNORECASE):
+            continue
+        # else:
+        #     print('Sorry, I did not understand that. Please type "yes" or "no".')
+
+    
+
+    balrog = '''
+      .:'                                 `:.
+     ::'                                   `::
+    :: :.                                .: ::
+    `:. `:.            .             .:' .:'
+    `::.`::            !           ::' .::'
+        `::.`::.    .' ! `.    .::'.::'
+            `:.  `::::'':!:``::::'   ::'
+            :'*:::.  .:' ! `:.  .:::*`:
+        :: HHH::.   ` ! '   .::HHH ::
+        ::: `H TH::.  `!'  .::HT H' :::
+        ::..  `THHH:`:   :':HHHT'  ..::
+        `::      `T: `. .' :T'      ::'
+            `:. .   :         :   . .:'
+            `::'               `::'
+               :'  .`.  .  .'.  `:
+               :' ::.       .:: `:
+               :' `:::     :::' `:
+                `.  ``     ''  .'
+                 :`...........':
+                 ` :`.     .': '
+                  `:  `"""'  :'
+    '''
+    print(
+        "Suddenly a Balrog appeared...\n",
+        balrog,
+        "\n'ROARR!!! \nIt said'",
+        )
+
+    response = input("What do you say? ")
+    print(get_result(response, hero))
+
 
 def get_hero(data):
     try:
@@ -56,6 +100,17 @@ def get_decision(data):
             continue
     return number
 
-
+def get_result(response, hero):
+    response = response.strip()
+    ends = ("crushes you", "eats you", "smashes you", "tramples on you", "burns you alive", "throws you into darkness")
+    races = ("Maiar", "Ainur")
+    if matches := re.match(r"^You\s+shall\s+not\s+pass+!*", response, re.IGNORECASE):
+        if hero.race in races:
+            return "\nThe barlog looks at you. It seems to understand. It steps back again... into the darkness.\nYOU WON.\n"
+        else:
+            return f"\nThe Barlog looks at you. It seems to understand, doubt creeps into its mind for a moment and then... he {random.choice(ends)}.\nYOU LOSE.\n"
+    else:
+        return f"\nThe balrog {random.choice(ends)}.\nYOU LOSE.\n"
+    
 if __name__ == "__main__":
     main()
